ca66525a6c4cbba9fa3e07021560c282d43dc730
Joseph Ampadu <jkampadujnr@gmail.com>
2024-08-02 14:42:35 +0000

    add cli subcommands

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,30 @@
+use clap::{arg, command, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("set")
+                .about("Set the value of a string key to a string")
+                .arg(arg!(<KEY>).required(true))
+                .arg(arg!(<VALUE>).required(true)),
+        )
+        .subcommand(
+            Command::new("get")
+                .about("Get the string value of a given string key")
+                .arg(arg!(<KEY>).required(true)),
+        )
+        .subcommand(
+            Command::new("rm")
+                .about("Remove a given key")
+                .arg(arg!(<KEY>).required(true)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("get", _)) => panic!("unimplemented"),
+        Some(("set", _)) => panic!("unimplemented"),
+        Some(("rm", _)) => panic!("unimplemented"),
+        _ => panic!(),
+    }
 }
