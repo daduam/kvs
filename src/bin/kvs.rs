79f3f64c030501b77a0151c2440de8fb54b4cfe1
Joseph Ampadu <jkampadujnr@gmail.com>
2024-08-03 15:49:05 +0000

    prefer clap_derive over clap_builder

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,34 +1,31 @@
-use clap::{arg, command, Command};
+use clap::{command, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = env!("CARGO_BIN_NAME"))]
+#[command(version, about, long_about = None, author)]
+#[command(arg_required_else_help = true)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Set the value of a string key to a string
+    Set { key: String, value: String },
+    /// Get the string value of a given string key
+    Get { key: String },
+    /// Remove a given key
+    Rm { key: String },
+}
 
 fn main() {
-    let matches = command!()
-        .name(env!("CARGO_BIN_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg_required_else_help(true)
-        .subcommand(
-            Command::new("set")
-                .about("Set the value of a string key to a string")
-                .arg(arg!(<KEY>).required(true))
-                .arg(arg!(<VALUE>).required(true)),
-        )
-        .subcommand(
-            Command::new("get")
-                .about("Get the string value of a given string key")
-                .arg(arg!(<KEY>).required(true)),
-        )
-        .subcommand(
-            Command::new("rm")
-                .about("Remove a given key")
-                .arg(arg!(<KEY>).required(true)),
-        )
-        .get_matches();
+    let cli = Cli::parse();
 
-    match matches.subcommand() {
-        Some(("get", _)) => panic!("unimplemented"),
-        Some(("set", _)) => panic!("unimplemented"),
-        Some(("rm", _)) => panic!("unimplemented"),
-        _ => panic!(),
+    match &cli.command {
+        Commands::Set { .. } => panic!("unimplemented"),
+        Commands::Get { .. } => panic!("unimplemented"),
+        Commands::Rm { .. } => panic!("unimplemented"),
     }
 }
